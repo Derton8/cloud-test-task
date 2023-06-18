7d9b792d46e61ff15eab831adf606795419b0a04
Danil Mysenko <danil.mysenko@yandex.ru>
2023-06-19 02:44:01 +0300

    Использовать css переменные

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 import { BrowserRouter } from 'react-router-dom';
-import './index.scss';
+import './styles/index.scss';
 import { Provider } from 'react-redux';
 import store from './store';
 
